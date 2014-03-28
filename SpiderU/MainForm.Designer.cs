c9f1da2e3e8220505e7a28af65852f405c7c1ee0
Hiromichi Tai <hiromichi.tai@kuramae.ne.jp>
2014-03-28 14:54:53 +0900

    Add menuitem to mainform

--- SpiderU/MainForm.Designer.cs
+++ SpiderU/MainForm.Designer.cs
@@ -26,11 +26,17 @@
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
 			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
 			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.scopeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.acquisitionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+			this.showHelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.scanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
 			this.menuStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -38,24 +44,20 @@
 			// 
 			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
             this.scopeToolStripMenuItem,
-            this.acquisitionToolStripMenuItem});
+            this.acquisitionToolStripMenuItem,
+            this.helpToolStripMenuItem2});
 			resources.ApplyResources(this.menuStrip1, "menuStrip1");
 			this.menuStrip1.Name = "menuStrip1";
 			// 
 			// fileToolStripMenuItem
 			// 
 			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.openToolStripMenuItem,
             this.toolStripMenuItem1,
             this.exitToolStripMenuItem});
 			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
 			resources.ApplyResources(this.fileToolStripMenuItem, "fileToolStripMenuItem");
-			// 
-			// openToolStripMenuItem
-			// 
-			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-			resources.ApplyResources(this.openToolStripMenuItem, "openToolStripMenuItem");
 			// 
 			// toolStripMenuItem1
 			// 
@@ -70,6 +72,9 @@
 			// 
 			// scopeToolStripMenuItem
 			// 
+			this.scopeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.scanToolStripMenuItem,
+            this.toolStripMenuItem2});
 			this.scopeToolStripMenuItem.Name = "scopeToolStripMenuItem";
 			resources.ApplyResources(this.scopeToolStripMenuItem, "scopeToolStripMenuItem");
 			// 
@@ -77,6 +82,47 @@
 			// 
 			this.acquisitionToolStripMenuItem.Name = "acquisitionToolStripMenuItem";
 			resources.ApplyResources(this.acquisitionToolStripMenuItem, "acquisitionToolStripMenuItem");
+			// 
+			// editToolStripMenuItem
+			// 
+			this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingToolStripMenuItem});
+			this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+			resources.ApplyResources(this.editToolStripMenuItem, "editToolStripMenuItem");
+			// 
+			// settingToolStripMenuItem
+			// 
+			this.settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+			resources.ApplyResources(this.settingToolStripMenuItem, "settingToolStripMenuItem");
+			// 
+			// helpToolStripMenuItem2
+			// 
+			this.helpToolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showHelpToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+			this.helpToolStripMenuItem2.Name = "helpToolStripMenuItem2";
+			resources.ApplyResources(this.helpToolStripMenuItem2, "helpToolStripMenuItem2");
+			// 
+			// showHelpToolStripMenuItem
+			// 
+			this.showHelpToolStripMenuItem.Name = "showHelpToolStripMenuItem";
+			resources.ApplyResources(this.showHelpToolStripMenuItem, "showHelpToolStripMenuItem");
+			// 
+			// aboutToolStripMenuItem
+			// 
+			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+			resources.ApplyResources(this.aboutToolStripMenuItem, "aboutToolStripMenuItem");
+			// 
+			// scanToolStripMenuItem
+			// 
+			this.scanToolStripMenuItem.Name = "scanToolStripMenuItem";
+			resources.ApplyResources(this.scanToolStripMenuItem, "scanToolStripMenuItem");
+			this.scanToolStripMenuItem.Click += new System.EventHandler(this.scanToolStripMenuItem_Click);
+			// 
+			// toolStripMenuItem2
+			// 
+			this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+			resources.ApplyResources(this.toolStripMenuItem2, "toolStripMenuItem2");
 			// 
 			// MainForm
 			// 
@@ -96,11 +142,17 @@
 
 		private System.Windows.Forms.MenuStrip menuStrip1;
 		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
 		private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
 		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem scopeToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem acquisitionToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem settingToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem2;
+		private System.Windows.Forms.ToolStripMenuItem showHelpToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem scanToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
 	}
 }
 
--- SpiderU/MainForm.cs
+++ SpiderU/MainForm.cs
@@ -10,11 +10,22 @@ using System.Windows.Forms;
 
 namespace SpiderU {
 	public partial class MainForm : Form {
+		DeviceListClass DeviceList;
+
 		public MainForm() {
 			InitializeComponent();
 		}
 
 		private void exitXToolStripMenuItem_Click(object sender, EventArgs e) {
+
+		}
+
+		private void scanToolStripMenuItem_Click(object sender, EventArgs e) {
+			if (DeviceList == null) {
+				DeviceList = new DeviceListClass();
+			}
+
+
 
 		}
 	}
