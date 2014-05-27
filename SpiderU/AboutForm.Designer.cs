1c64673f3c48dfe2cc003a880a2061f5b1a91da9
Hiromichi Tai <hiromichi.tai@toshiba.co.jp>
2014-05-27 17:59:42 +0900

    Add aboutform

---
+++ SpiderU/AboutForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace SpiderU {
+	partial class AboutForm {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.OKButton = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// OKButton
+			// 
+			this.OKButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.OKButton.Location = new System.Drawing.Point(101, 190);
+			this.OKButton.Name = "OKButton";
+			this.OKButton.Size = new System.Drawing.Size(75, 23);
+			this.OKButton.TabIndex = 0;
+			this.OKButton.Text = "OK";
+			this.OKButton.UseCompatibleTextRendering = true;
+			this.OKButton.UseVisualStyleBackColor = true;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(45, 30);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(201, 120);
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// AboutForm
+			// 
+			this.AcceptButton = this.OKButton;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.OKButton);
+			this.Name = "AboutForm";
+			this.Text = "About SpiderU";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button OKButton;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
