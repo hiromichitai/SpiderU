64b48339d5072303b1f7329e5399e9210668bce1
Hiromichi Tai <hiromichi.tai@kuramae.ne.jp>
2014-07-14 22:32:46 +0900

    some documents are changed?

--- SpiderU/AboutForm.Designer.cs
+++ SpiderU/AboutForm.Designer.cs
@@ -36,76 +36,50 @@
 			// OKButton
 			// 
 			this.OKButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.OKButton.Location = new System.Drawing.Point(137, 273);
-			this.OKButton.Margin = new System.Windows.Forms.Padding(4);
+			resources.ApplyResources(this.OKButton, "OKButton");
 			this.OKButton.Name = "OKButton";
-			this.OKButton.Size = new System.Drawing.Size(100, 29);
-			this.OKButton.TabIndex = 0;
-			this.OKButton.Text = "OK";
 			this.OKButton.UseCompatibleTextRendering = true;
 			this.OKButton.UseVisualStyleBackColor = true;
 			// 
 			// pictureBox1
 			// 
-			this.pictureBox1.Location = new System.Drawing.Point(60, 33);
-			this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+			resources.ApplyResources(this.pictureBox1, "pictureBox1");
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(52, 44);
-			this.pictureBox1.TabIndex = 1;
 			this.pictureBox1.TabStop = false;
 			// 
 			// label1
 			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(57, 108);
+			resources.ApplyResources(this.label1, "label1");
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(61, 15);
-			this.label1.TabIndex = 2;
-			this.label1.Text = "SpiderU ";
 			this.label1.Click += new System.EventHandler(this.label1_Click);
 			// 
 			// label2
 			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(57, 134);
+			resources.ApplyResources(this.label2, "label2");
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(294, 15);
-			this.label2.TabIndex = 3;
-			this.label2.Text = "Copyright(C) 2014 Ryuusando,　Hiromichi Tai";
 			// 
 			// versionLabel
 			// 
-			this.versionLabel.AutoSize = true;
-			this.versionLabel.Location = new System.Drawing.Point(187, 108);
+			resources.ApplyResources(this.versionLabel, "versionLabel");
 			this.versionLabel.Name = "versionLabel";
-			this.versionLabel.Size = new System.Drawing.Size(0, 15);
-			this.versionLabel.TabIndex = 4;
 			// 
 			// label3
 			// 
-			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(124, 108);
+			resources.ApplyResources(this.label3, "label3");
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(53, 15);
-			this.label3.TabIndex = 5;
-			this.label3.Text = "version";
 			// 
 			// AboutForm
 			// 
 			this.AcceptButton = this.OKButton;
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-			this.ClientSize = new System.Drawing.Size(379, 328);
+			resources.ApplyResources(this, "$this");
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.versionLabel);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.OKButton);
-			this.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Margin = new System.Windows.Forms.Padding(4);
 			this.Name = "AboutForm";
-			this.Text = "About SpiderU";
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
