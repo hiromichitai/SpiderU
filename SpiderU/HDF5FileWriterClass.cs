f823ca545f733b5a088392ccce1d094a08604b4f
Hiromichi Tai <hiromichi.tai@kuramae.ne.jp>
2014-07-24 23:35:33 +0900

    making HDF5FileWriterClass

--- SpiderU/HDF5FileWriterClass.cs
+++ SpiderU/HDF5FileWriterClass.cs
@@ -38,29 +38,30 @@ namespace SpiderU {
 					string groupIDString = string.Format("Oscilloscope{0:d}", SIndex + 1);
 					H5GroupId groupID = H5G.create(fileID, groupIDString);
 
-					long[] dimension = new long[2];
-					dimension[0] = Scope.OnTrace.Count+1;
-					dimension[1] = Scope.DataLength;
-					H5DataSpaceId spaceId = H5S.create_simple(1, dimension);
-					H5DataTypeId typeId = H5T.copy(H5T.H5Type.NATIVE_DOUBLE);
+					long[] dataDimension = new long[2];
+					dataDimension[0] = Scope.OnTrace.Count+1;
+					dataDimension[1] = Scope.DataLength;
+					H5DataSpaceId dataSpaceId = H5S.create_simple(1, dataDimension);
+					H5DataTypeId doubleTypeId = H5T.copy(H5T.H5Type.NATIVE_DOUBLE);
 
-					H5T.setOrder(typeId, H5T.Order.LE);
+					H5T.setOrder(doubleTypeId, H5T.Order.LE);
 					H5DataTypeId UCharTypeID = H5T.copy(H5T.H5Type.NATIVE_UCHAR);
 					long [] labelDimension = new long[1];
 					long [] unitDimension = new long[1];
 
 					for (int TIndex = 0; TIndex < Scope.OnTrace.Count; TIndex++) {
+
 						labelDimension[0] = Scope.ChannelLabel(TIndex).Length;
 						H5DataSpaceId labelSpaceID = H5S.create_simple(1, labelDimension);
 						unitDimension[0] = Scope.ChannelUnit(TIndex).Length;
 						H5DataSpaceId unitSpaceID = H5S.create_simple(1, unitDimension);
 						
-						H5DataSetId dataSetID = H5D.create(fileID, "/" + Scope.ChannelLabel(TIndex) , typeId, spaceId);
+						H5DataSetId dataSetID = H5D.create(fileID, "/" + Scope.ChannelLabel(TIndex) , doubleTypeId, dataSpaceId);
 						H5AttributeId label = H5A.create(dataSetID, "label",  UCharTypeID, labelSpaceID);
-
-//						H5A.write<char>(label, H5T.H5Type.NATIVE_UCHAR, new H5Array<char>(Scope.OnTrace[TIndex].TraceLabel));
-
-//						H5D.write(dataSetID, new H5Array<double>(Scope.OnTrace[TIndex].Data));
+						H5A.write<char>(label,UCharTypeID,new H5Array<char>(Scope.OnTrace[TIndex].TraceLabel.ToCharArray()));
+						H5AttributeId unit = H5A.create(dataSetID, "unit", UCharTypeID, unitSpaceID);
+						H5A.write<char>(label, UCharTypeID, new H5Array<char>(Scope.OnTrace[TIndex].TraceUnit.ToCharArray()));
+						H5D.write<double>(dataSetID, doubleTypeId, new H5Array<double>(Scope.OnTrace[TIndex].Data));
 						H5D.close(dataSetID);
 					}
 					H5G.close(groupID);
